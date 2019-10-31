9a41c7bbad5667596a866846b3acb02496863cbe
radius <b1916320@planet.kanazawa-it.ac.jp>
2019-11-01 04:41:48 +0900

    add:easy code for explanation

---
+++ xR Project/Assets/Script/DistanceUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceUI : MonoBehaviour
+   
+{
+    public Transform PlayerPosition;
+    public float player_obj_distance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Text myText = this.GetComponent<Text>();
+        if (Vector3.Distance(transform.position, PlayerPosition.transform.position) >= player_obj_distance)
+        {
+            myText.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+            
+        }
+        else
+        {
+            myText.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+            
+        }
+    }
+}
