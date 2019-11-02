1af165bf6eb44f31ed0938611e99bd27c043ea29
radius <b1916320@planet.kanazawa-it.ac.jp>
2019-11-02 09:06:30 +0900

    add; Canvs script

---
+++ xR Project/Assets/Script/Canvs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvs : MonoBehaviour
+
+{
+    public Transform PlayerPosition;
+    public float player_obj_distance;
+    public GameObject MyPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Text myText = this.GetComponent<Text>();
+        if (Vector3.Distance(transform.position, PlayerPosition.transform.position) >= player_obj_distance)
+        {
+            MyPanel.SetActive(false);
+
+        }
+        else
+        {
+            MyPanel.SetActive(true);
+
+        }
+    }
+}
